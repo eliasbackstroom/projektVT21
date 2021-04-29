ee2bb61900edc2b4a753a26a14e7c3d6c6322d00
Elias Bäckström <elias.backstrom@edu.lycksele.se>
2021-04-29 15:26:28 +0200

    Been finishing the project and commenting the code

--- 2-Sandlåda-Skrivtest.py
+++ 2-Sandlåda-Skrivtest.py
@@ -3,17 +3,22 @@ import random
 import time 
 from difflib import SequenceMatcher
 
+#Skrivtest där du kan testa din skrivhastighet
+
+
+#Denna funktion startar timern. 
 def starttimer():
     start_time = time.time()
     return start_time
 
+#Denna funktion stoppar timern och räknar ut totala tiden.
 def endtimer(start_time):
     end_time = time.time()
     
     total_time = int(end_time - start_time)   
     return total_time
     
-    
+    #Denna funktion innehåller startmenyn. 
 def writing():
     print("Välkommen att Spela!")
     print("Skriv meningen så snabbt du kan")
@@ -23,49 +28,50 @@ def writing():
         startgame(choose)
     if begin == "2":
         print("Spelet går ut på att du ska skriva meningen som kommer upp så snabbt som möjligt.")
+        time.sleep(1)
         print("Efter du skrivit meningen kommer du få resultat i form av ord per minut och träffsäkerhet")
+        time.sleep(1)
         begin = input("För att komma till menyn igen tryck 1 följt av Enter:")
         if begin == "1":
             startgame(choose)
     if begin == "3":
         choose = choosefiles()
         startgame(choose)
-        
     
-    
+#Här sker kategoribytet.
 def choosefiles():
-    choose = input("Välj vilket tema du vill skriva på. Skriv Quotes eller Böcker för att välja:")
+    choose = input("Välj vilket tema du vill skriva på. Skriv Citat eller Ordspråk för att välja:")
     choose = choose.capitalize()
-    if choose != "böcker" or choose != "Böcker":
-        choose = input("Något blev fel, var snäll och skriv igen")
+    while not (choose == "Citat" or choose == "Ordspråk"):
+        choose = input("Något blev fel, var snäll och skriv igen:")
     return choose
-    
+
+#Här öppnas kategorin.
 def randomsentence(choose):
-    with open(str(choose) + ".txt","r") as file: 
+    with open(str(choose) + ".txt","r") as file: #Txt filen med meningarna läses
         allText = file.read()
-        words = list(map(str, allText.split(".")))
+        words = list(map(str, allText.split("."))) #Meningarna delas efter varje punkt.
         
-        sentence = (random.choice(words))
+        sentence = (random.choice(words)) #Meningen slumpas ut.
 
     return sentence
        
-       
+ #Här sker sammanställningen av all data.      
 def comparison():
-    last10 = []
     time = timer()
     Sentence = writing()
     print("Time:",time,"seconds")    
-    total_words = len(Sentence.split())
+    total_words = len(Sentence.split()) #För att ta reda på antal ord delas meningen upp efter mellanslagen.
     wpmtime = 60 // time
     wpm = wpmtime*total_words
     print("Wpm:",wpm)        
-    last10.append(wpm)
     
+#Användarens input i form av meningen.
 def answer_sentence():
     written_sentence = input("Tryck Enter när du är klar:")
     return written_sentence
     
-    
+#När användaren väljer att starta spelet körs denna funktion.
 def startgame(choose):
     print("Då kör vi!")
     time.sleep(1)
@@ -81,8 +87,8 @@ def startgame(choose):
     start_time = starttimer()
     answer = answer_sentence()
     endtime = endtimer(start_time)
-    ratio = SequenceMatcher(None, Original_sentence, answer).ratio()
-    
+    ratio = SequenceMatcher(None, Original_sentence, answer).ratio() #Jämförelsen av meningarna ger en förändringsfaktor
+    #Resultatet skrivs ut
     print("-"*50)
     print("Accuracy:", int(ratio*100),"%")
     print("Time:", endtime,"seconds")
